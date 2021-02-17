1abd8ecb8da67d2c4f19a2f31476fb98daf210ae
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-16 22:55:22 -0300

    Estacionamento em TypeScript.

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Estacionamento | TypeScript :blue_car: :red_car:</h1>
+
+<p align="center">
+  <a href="#">
+    <img 
+         src="#" 
+         alt="Estacionamento" 
+    />
+  </a>
+  <br />
+  <i>Sistema de Estacionamento Usando TypeScript.</i>
+</p>
+
+<hr />
+
+<details>
+  <summary><strong>Fonte</strong></summary>
+    <br />
+    <p align="left">
+        Plataforma: <a href="https://web.digitalinnovation.one/home">Digital Innovation One.</a>
+        <br /> 
+        Desafio: <a href="https://web.digitalinnovation.one/lab/aprenda-a-criar-um-sistema-de-estacionamento-usando-typescript/learning/8a299479-4165-4e7f-b7f6-55a7b548c9ef">Aprenda a criar um sistema de estacionamento usando TypeScript.</a>
+    </p>
+</details>
+
+<hr />
---
+++ src/main.ts
@@ -0,0 +1,138 @@
+interface Carro {
+  nome: string;
+  placa: string;
+  entrada: Date | string;
+}
+
+interface Encerrar {
+  nome: string;
+  placa: string;
+  tempo: number;
+}
+
+class EstacionamentoFront {
+  constructor(
+    private $: (q: string) => HTMLInputElement,
+    private estacionamento = new Estacionamento()
+  ) {}
+
+  adicionar(carro: Carro, salvar = false) {
+    this.estacionamento.adicionar(carro);
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+                <td>${carro.nome}</td>
+                <td>${carro.placa}</td>
+                <td data-time="${carro.entrada}">
+                    ${carro.entrada.toLocaleString("pt-BR", {
+                      hour: "numeric",
+                      minute: "numeric",
+                    })}
+                </td>
+                <td>
+                    <button class="delete">x</button>
+                </td>
+            `;
+
+    if (salvar) {
+      this.estacionamento.salvar();
+    }
+
+    this.$("#garage").appendChild(row);
+  }
+
+  encerrar(cells: HTMLCollection) {
+    if (cells[2] instanceof HTMLElement) {
+      const veiculo = {
+        nome: cells[0].textContent || "",
+        placa: cells[1].textContent || "",
+        tempo:
+          new Date().valueOf() -
+          new Date(cells[2].dataset.time as string).valueOf(),
+      };
+
+      this.estacionamento.encerrar(veiculo);
+    }
+  }
+
+  render() {
+    this.$("#garage").innerHTML = "";
+    this.estacionamento.patio.forEach((c) => this.adicionar(c));
+  }
+}
+
+class Estacionamento {
+  public patio: Carro[];
+  constructor() {
+    this.patio = localStorage.patio ? JSON.parse(localStorage.patio) : [];
+  }
+
+  adicionar(carro: Carro) {
+    this.patio.push(carro);
+  }
+
+  encerrar(info: Encerrar) {
+    const tempo = this.calcTempo(info.tempo);
+
+    const msg = `
+      O veículo ${info.nome} de placa ${info.placa} permaneceu ${tempo} estacionado.
+      Deseja encerrar?
+    `;
+
+    if (!confirm(msg)) return;
+
+    this.patio = this.patio.filter((carro) => carro.placa !== info.placa);
+
+    this.salvar();
+  }
+
+  private calcTempo(mil: number) {
+    var min = Math.floor(mil / 60000);
+    var sec = Math.floor((mil % 60000) / 1000);
+
+    return `${min}m e ${sec}s`;
+  }
+
+  salvar() {
+    console.log("Salvando...");
+    localStorage.patio = JSON.stringify(this.patio);
+  }
+}
+
+(function () {
+  const $ = (q: string) => {
+    const elem = document.querySelector<HTMLInputElement>(q);
+
+    if (!elem) throw new Error("Ocorreu um erro ao buscar o elemento.");
+
+    return elem;
+  };
+
+  const estacionamento = new EstacionamentoFront($);
+  estacionamento.render();
+
+  $("#send").addEventListener("click", () => {
+    const nome = $("#name").value;
+    const placa = $("#licence").value;
+
+    if (!nome || !placa) {
+      alert("Os campos são obrigatórios.");
+      return;
+    }
+
+    const carro: Carro = { nome, placa, entrada: new Date().toISOString() };
+
+    estacionamento.adicionar(carro, true);
+
+    $("#name").value = "";
+    $("#licence").value = "";
+  });
+
+  $("#garage").addEventListener("click", ({ target }: MouseEvent | any) => {
+    if (target.className === "delete") {
+      estacionamento.encerrar(target.parentElement.parentElement.cells);
+      estacionamento.render();
+    }
+  });
+
+})();
